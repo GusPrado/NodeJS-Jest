ba943d6be501d8fbb8fd9a32901ab5132219a52e
Gus Prado <contato@gusprado.com>
2019-12-04 22:02:50 -0300

    Added more tests

--- __tests__/integration/user.test.js
+++ __tests__/integration/user.test.js
@@ -1,16 +1,46 @@
 import request from 'supertest';
+import bcrypt from 'bcryptjs';
+
+import factory from '../factories';
 import app from '../../src/app';
+import truncate from '../util/truncate';
 
 describe('User', () => {
+  beforeEach(async () => {
+    await truncate();
+  });
+
+  it('should encrypt user passwd when new user created', async () => {
+    const user = await factory.create('User', {
+      password: '123456',
+    });
+
+    const compareHash = await bcrypt.compare('123456', user.password_hash);
+
+    expect(compareHash).toBe(true);
+  });
+
   it('should be able to register', async () => {
+    const user = await factory.attrs('User');
+
     const res = await request(app)
       .post('/users')
-      .send({
-        name: 'Gus Prado',
-        email: 'gus@gusprado.com',
-        password_hash: '123456',
-      });
+      .send(user);
 
     expect(res.body).toHaveProperty('id');
+  });
+
+  it('should not be able to register with duplicated email', async () => {
+    const user = await factory.attrs('User');
+
+    await request(app)
+      .post('/users')
+      .send(user);
+
+    const res = await request(app)
+      .post('/users')
+      .send(user);
+
+    expect(res.status).toBe(400);
   });
 });
